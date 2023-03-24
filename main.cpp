f16b16bfd252d04a13feb8c0989b3fc87aa6c7f4
Sergei Barbarash <sgt.sgt@gmail.com>
2023-03-25 02:53:30 +0300

    🌼

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <numeric>
 #include <optional>
+#include <functional>
 
 #include "doctest.h"
 
@@ -60,6 +61,15 @@ class Solution {
 public:
     static int add(int x, int y) { return x + y; }
 
+    // A companion function for guess-a-number.
+    static function<int(int)> guess_fn(int pick) {
+        return [=](int num) {
+            if (num > pick) return -1;
+            else if (num < pick) return 1;
+            else return 0;
+        };
+    }
+
     // 21. Merge Two Sorted Lists
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
         // TODO can be rewritten to make the merge in-place
@@ -104,32 +114,85 @@ public:
 
     // 206. Reverse Linked List
     ListNode *reverseList(ListNode *head) {
-        if (!head) return head;
+        if (!head) return nullptr;
         auto curr = head;
-        auto prev = nullptr;
+        ListNode *prev = nullptr;
         while (curr) {
-
-            auto next = head->next;
+            auto next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
+        return prev;
     }
 
     // 704. Binary Search
     int search(vector<int> &nums, int target) {
-
+        if (nums.empty()) return -1;
+        int low{0}, high{static_cast<int>(nums.size() - 1)};
+        while (low <= high) {
+            int i{(low + high) / 2};
+            if (nums[i] > target) {
+                high = i - 1;
+            } else if (nums[i] < target) {
+                low = i + 1;
+            } else {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // 374. Guess Number Higher or Lower
-    int guessNumber(int n) {
-
+    int guessNumber(int n, const function<int(int)> &guess) {
+        int low{1}, high{n};
+        while (low != high) {
+            int g{(low + high) / 2};
+            int r{guess(g)};
+            if (r == -1) {
+                high = g - 1;
+            } else if (r == 1) {
+                low = g + 1;
+            } else {
+                return g;
+            }
+        }
+        return low;
     }
 
     // 74. Search a 2D Matrix
     bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        int m{static_cast<int>(matrix.size())}, n{static_cast<int>(matrix[0].size())};
+        auto get_val{
+                [=](size_t val) {
+                    size_t nc{val % n}, mc{val / n};
+                    return matrix[mc][nc];
+                }
+        };
+        int low{0}, high{m * n - 1};
+        while (low <= high) {
+            int i{(low + high) / 2};
+            int cur{get_val(i)};
+            if (cur > target) {
+                high = i - 1;
+            } else if (cur < target) {
+                low = i + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    // 136. Single Number
+    int singleNumber(vector<int> &nums) {
+        return accumulate(nums.cbegin(), nums.cend(), 0, [](int acc, int i) { return acc ^ i; });
     }
 };
 
 TEST_CASE("Test all") {
+    Solution s{};
+
     SUBCASE("Silly test") {
         CHECK(Solution::add(2, 3) == 5);
     }
@@ -147,15 +210,56 @@ TEST_CASE("Test all") {
     }
 
     SUBCASE("Merge two lists") {
-        Solution s{};
         CHECK(listNode_to_vector(s.mergeTwoLists(vector_to_listNode({2, 4}), vector_to_listNode({1, 3}))) ==
               vector<int>{1, 2, 3, 4});
     }
 
     SUBCASE("Merge K lists") {
-        Solution s{};
         vector<ListNode *> lists = {vector_to_listNode({2, 4}), vector_to_listNode({4, 5}), vector_to_listNode({1, 3})};
         CHECK(listNode_to_vector(s.mergeKLists(lists)) == vector<int>{1, 2, 3, 4, 4, 5});
+    }
+
+    SUBCASE("Reverse linked list") {
+        CHECK(listNode_to_vector(s.reverseList(vector_to_listNode({1, 2, 3}))) == vector<int>{3, 2, 1});
+        CHECK(listNode_to_vector(s.reverseList(vector_to_listNode({1, 2}))) == vector<int>{2, 1});
+        CHECK(listNode_to_vector(s.reverseList(vector_to_listNode({}))) == vector<int>{});
+        CHECK(listNode_to_vector(s.reverseList(vector_to_listNode({1}))) == vector<int>{1});
+    }
+
+    SUBCASE("Binary search") {
+        vector<int> v{-1, 0, 3, 5, 9, 12};
+        CHECK(s.search(v, 9) == 4);
+        CHECK(s.search(v, 2) == -1);
+        v = {5};
+        CHECK(s.search(v, 5) == 0);
+        CHECK(s.search(v, 10) == -1);
+        CHECK(s.search(v, 0) == -1);
+    }
+
+    SUBCASE("Guess number") {
+        CHECK(s.guessNumber(10, Solution::guess_fn(6)) == 6);
+        CHECK(s.guessNumber(1, Solution::guess_fn(1)) == 1);
+        CHECK(s.guessNumber(2, Solution::guess_fn(1)) == 1);
+    }
+
+    SUBCASE("Search a 2D matrix") {
+        vector<vector<int>> v = {{1,  3,  5,  7},
+                                 {10, 11, 16, 20},
+                                 {23, 30, 34, 60}};
+        CHECK(s.searchMatrix(v, 3) == true);
+        CHECK(s.searchMatrix(v, 13) == false);
+        v = {{1},
+             {2},
+             {3},
+             {4}};
+        CHECK(s.searchMatrix(v, 3) == true);
+        CHECK(s.searchMatrix(v, 13) == false);
+        CHECK(s.searchMatrix(v, -1) == false);
+    }
+
+    SUBCASE("Single number") {
+        vector<int> v = {4, 1, 2, 1, 2};
+        CHECK(s.singleNumber(v) == 4);
     }
 }
 
